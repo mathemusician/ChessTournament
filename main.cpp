a927bd0082199bd64eb267a21511a4b883f8f0c8
Jv Kyle Eclarin <jvkyleeclarin@gmail.com>
2021-03-30 21:53:34 -0500

    Got the starting files ready

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to the chess tournament!" << endl;
+    cout << "Loading previous game data..." << endl;
+
+    
+
+
+
+    return 0;
+}
